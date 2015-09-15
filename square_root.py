ed1ba63b0f3bb07ae91aed682d569875319c2cd0
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-15 14:39:18 -0400

    sqare_root.py

--- square_root.py
+++ square_root.py
@@ -5,8 +5,7 @@ def approx_sqrt(num):
     guess = float(randint(1,num))
     previous_guess = 0.0
     while float("{0:.3}".format(previous_guess)) != float("{0:.3}".format(guess)):
-        previous_guess = guess
-        guess = (1/2)*(guess+(num/guess))
+        previous_guess, guess = guess, (1/2)*(guess+(num/guess))
     return guess
 
 print(approx_sqrt(int(input('What number do you want to square root? '))))
