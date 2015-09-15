46423251bd230ee105d8a08b131f95e09cf478c7
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-15 15:03:05 -0400

    square_root.py

--- square_root.py
+++ square_root.py
@@ -4,8 +4,12 @@ from random import randint
 def approx_sqrt(num):
     guess = float(randint(1,num))
     previous_guess = 0.0
+    iteration = 0
+    iteration_guess = []
     while float("{0:.3}".format(previous_guess)) != float("{0:.3}".format(guess)):
         previous_guess, guess = guess, (1/2)*(guess+(num/guess))
-    return guess
+        iteration +=1
+        iteration_guess.append([iteration, guess])
+    return iteration_guess
 
 print(approx_sqrt(int(input('What number do you want to square root? '))))
