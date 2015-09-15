9aae0df13cb5be23cb0a3d179a7dd9e1537bae77
Zach Seaborn <zseaborn63@gmail.com>
2015-09-15 15:22:03 -0400

    Normal Mode complete

--- square_root.py
+++ square_root.py
@@ -1,7 +1,16 @@
-# Going to make a program that guesses the square root of a number
+# Going to make a program that guesses the square root of a number using Newton's Method 
+
 
 user_num = input("Enter a positive number: ")
-epsilon = 0.01
+guess = user_num / 2
+error = 1
+
+While error > 0.01:
+    x = (guess + (user_num/guess))*(1/2)
+    error = guess - x
+    guess = x
+print("The square root of {} is {}").format(user_num, guess)
+    
 
 
 
