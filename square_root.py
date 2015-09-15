ad5a0006e0a60f26cd779c9d8d084fab20d929cd
Zach Seaborn <zseaborn63@gmail.com>
2015-09-15 16:12:17 -0400

    Hard Mode completed

--- square_root.py
+++ square_root.py
@@ -1,29 +1,24 @@
 # Going to make a program that guesses the square root of a number using Newton's Method 
-
+# Hard Mode enables input recognition, and includes an iteration counter and lists guesses
 
 user_num = input("Enter a positive number: ")
 
-if user_num < 0:
-    print("No negative numbers!")
+try:
+    user_num = float(user_num)
+    if user_num < 0:
+        print("No negative numbers!")
+    guess = user_num/2
+    error = 1
+    counter = 0
+    while error > 0.01:
+        x = (guess+(user_num/guess))*(1/2)
+        error = guess - x
+        counter += 1
+        print("Current number of iterations: {}".format(counter))
+        print("Current guess is: {}".format(guess))
+        guess = x
+    print("The square root of {} is {}.".format(user_num, guess))
 
-user_num = float(user_num)
-guess = user_num / 2
-error = 1
-counter = 0
-iteration_list = []
-guess_list = []
-
-While error > 0.01:
-    x = (guess + (user_num/guess))*(1/2)
-    error = guess - x
-    guess_list.append(guess)
-    counter += 1
-    iteration_list.append(counter)
-    print("Current number of iterations: {}".format(len(iteration_list)))
-    print("Current guess is: {}".format(guess))
-    guess = x
-print("The square root of {} is {}").format(user_num, guess)
-    
-
-
+except:
+    print("Numbers only!") 
 
