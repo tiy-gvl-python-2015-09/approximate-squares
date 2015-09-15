b88bbc0424b0e04d4ac13df823ff469755c7d049
andbar <andbar@users.noreply.github.com>
2015-09-15 16:31:36 -0400

    Hard mode finished

--- square_root.py
+++ square_root.py
@@ -1,22 +1,12 @@
-from random import randint
-from math import ceil
-
-def approximate_square_root():
-    user_number = float(input("Enter a positive number: "))
-    guess = float(randint(0, ceil(user_number)))
-    
-    while abs((user_number - (guess**2))) > 0.01:
-        new_guess = (guess + (user_number/guess))/2
-        guess = new_guess
-    print("The square root of {} is {}".format(user_number, guess))
-
-approximate_square_root()
-
-
 def better_approximate_square_root():
-    user_input = input("Enter a positive number: ")
-    
-    if float(user_input) < 0:
+    user_input = input("Enter a positive number: ") # Prompts the user for a number
+    user_input_no_dec = user_input.replace(".", "") # Removes the decimal if not a whole number
+    user_input_no_neg = user_input_no_dec.replace("-", "") # Removes the minus if negative number
+
+    if user_input_no_neg.isdigit() == False: # Checks user input to make sure they put in a number
+        print("That's not a number. Please try again.")
+        better_approximate_square_root()
+    elif float(user_input) < 0:
         print("That's a negative number. Please try again.")
         better_approximate_square_root()
     else:
@@ -32,4 +22,5 @@ def better_approximate_square_root():
         
         print("The square root of {} is {}".format(user_number, guess))
         
+
 better_approximate_square_root()
