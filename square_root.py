14bdc4a2bbc33f70b490d42f2e216a25e6508a18
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-15 14:34:33 -0400

    sqare_root.py

--- square_root.py
+++ square_root.py
@@ -2,12 +2,11 @@ import math
 from random import randint
 
 def approx_sqrt(num):
-    random_num = float(randint(1,num))
-    previous_random_num = 0.0
-    while float("{0:.3}".format(previous_random_num)) != float("{0:.3}".format(random_num)):
-        previous_random_num = random_num
-        error = (num - random_num**2)/2*random_num
-        random_num = (1/2)*(random_num+(num/random_num))
-    return random_num
+    guess = float(randint(1,num))
+    previous_guess = 0.0
+    while float("{0:.3}".format(previous_guess)) != float("{0:.3}".format(guess)):
+        previous_guess = guess
+        guess = (1/2)*(guess+(num/guess))
+    return guess
 
 print(approx_sqrt(int(input('What number do you want to square root? '))))
