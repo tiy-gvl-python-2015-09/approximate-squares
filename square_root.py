8e6594fd0ffb982cb5c8b947957f0c3ed49de881
andbar <andbar@users.noreply.github.com>
2015-09-15 15:19:35 -0400

    Hard Mode Part 1 Done

--- square_root.py
+++ square_root.py
@@ -11,3 +11,19 @@ def approximate_square_root():
     print("The square root of {} is {}".format(user_number, guess))
 
 approximate_square_root()
+
+
+def better_approximate_square_root():
+    user_number = float(input("Enter a positive number: "))
+    guess = user_number/2
+    count = 1
+    
+    while abs((user_number - (guess**2))) > 0.01:
+        print("Square root guess number {}: {}".format(count, guess))
+        new_guess = (guess + (user_number/guess))/2
+        guess = new_guess
+        count += 1
+        
+    print("The square root of {} is {}".format(user_number, guess))
+
+better_approximate_square_root()
